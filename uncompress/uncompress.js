95a3b11e5c0347ee52a3d98f943657617128d474
Sean Hoang <116997524+SonQHoang@users.noreply.github.com>
2023-12-05 18:24:01 -0800

    Solved 'uncompress' (.js)

---
+++ uncompress/uncompress.js
@@ -0,0 +1,24 @@
+const uncompress = (s) => {
+  let result = [];
+  const numbers = '0123456789';
+  let i = 0;
+  let j = 0;
+  while (j < s.length) {
+    if (numbers.includes(s[j])) {
+      j++;
+    } else {
+      let num = Number(s.slice(i, j));
+      for (let count = 0; count < num; count += 1) {
+        result.push(s[j]);
+      }
+      j += 1;
+      i = j;
+    }
+  }
+  return result.join('');
+};
+​
+module.exports = {
+  uncompress,
+};
+​
